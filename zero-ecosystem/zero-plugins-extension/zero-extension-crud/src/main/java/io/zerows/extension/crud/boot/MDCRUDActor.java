7960b7c48a60a7e51622f3b7f3521926b3ccb94a
Lang <silentbalanceyh@126.com>
2025-12-24 00:53:52 +0800

    CRUD

--- zero-ecosystem/zero-plugins-extension/zero-extension-crud/src/main/java/io/zerows/extension/crud/boot/MDCRUDActor.java
+++ zero-ecosystem/zero-plugins-extension/zero-extension-crud/src/main/java/io/zerows/extension/crud/boot/MDCRUDActor.java
@@ -10,7 +10,7 @@ import io.zerows.specification.configuration.HConfig;
  * @author lang : 2025-12-16
  */
 @Actor(value = "extension", sequence = 200)
-public class ModCrudActor extends ExAbstractHActor {
+public class MDCRUDActor extends ExAbstractHActor {
     @Override
     protected Future<Boolean> startAsync(final HConfig config, final Vertx vertxRef) {
         return Future.succeededFuture(Boolean.TRUE);
