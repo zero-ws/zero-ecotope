02b3bf654a437e90fa3a80849a0fd5983d1e3585
Lang <silentbalanceyh@126.com>
2025-12-26 21:41:09 +0800

    Excel 导入问题

--- zero-ecosystem/zero-plugins-equip/zero-plugins-excel/src/main/java/io/zerows/plugins/excel/component/ExValueExpr.java
+++ zero-ecosystem/zero-plugins-equip/zero-plugins-excel/src/main/java/io/zerows/plugins/excel/component/ExValueExpr.java
@@ -45,7 +45,7 @@ public class ExValueExpr implements ExValue {
                 this.put(ExcelConstant.CELL.CODE_NAME_CONFIG, (pathRoot, paramMap) -> {
                     final String code = paramMap.get(KName.CODE);
                     final String name = paramMap.get(KName.NAME);
-                    return Ut.ioPath(pathRoot, code) + File.pathSeparator + name;
+                    return Ut.ioPath(pathRoot, code) + File.separator + name;
                 });
                 // CODE:class
                 this.put(ExcelConstant.CELL.CODE_CLASS, (pathRoot, paramMap) -> {
