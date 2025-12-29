984b8ac11b5f9d4e14eebb64a42d00339e634032
Lang <silentbalanceyh@126.com>
2025-12-29 12:18:19 +0800

    Monitor

--- zero-ecosystem/zero-plugins-equip/zero-plugins-monitor-prometheus/src/main/java/io/zerows/plugins/monitor/prometheus/MonitorEquipPrometheus.java
+++ zero-ecosystem/zero-plugins-equip/zero-plugins-monitor-prometheus/src/main/java/io/zerows/plugins/monitor/prometheus/MonitorEquipPrometheus.java
@@ -4,6 +4,7 @@ import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxPrometheusOptions;
+import io.zerows.epoch.constant.KName;
 import io.zerows.plugins.monitor.server.MonitorEquipBase;
 import io.zerows.support.Ut;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,9 @@ public class MonitorEquipPrometheus extends MonitorEquipBase {
     public void registryOption(final JsonObject serverJ, final MicrometerMetricsOptions micrometerOptions) {
         // 不需要再判断 prometheus-enabled 开关了，依赖存在即开启
         // 当然，你也可以保留开关作为“依赖存在但依然想手动关闭”的后门
-
-        final int port = Ut.valueInt(serverJ, "prometheus-port", 9090);
-        final String path = Ut.valueString(serverJ, "prometheus-path", "/metrics");
+        final JsonObject configJ = Ut.valueJObject(serverJ, "monitor-config");
+        final int port = Ut.valueInt(configJ, KName.PORT, 9090);
+        final String path = Ut.valueString(configJ, KName.PATH, "/metrics");
 
         micrometerOptions.setPrometheusOptions(new VertxPrometheusOptions()
             .setEnabled(true)
