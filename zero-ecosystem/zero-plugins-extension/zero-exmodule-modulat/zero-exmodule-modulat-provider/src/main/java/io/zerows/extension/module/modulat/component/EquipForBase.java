9611037444fdcf8f6b34c8f12ce3f6d099e5bfed
Lang <silentbalanceyh@126.com>
2026-02-21 22:25:25 +0800

    两种不同方式的配置数据提取

--- zero-ecosystem/zero-plugins-extension/zero-exmodule-modulat/zero-exmodule-modulat-provider/src/main/java/io/zerows/extension/module/modulat/component/EquipForBase.java
+++ zero-ecosystem/zero-plugins-extension/zero-exmodule-modulat/zero-exmodule-modulat-provider/src/main/java/io/zerows/extension/module/modulat/component/EquipForBase.java
@@ -1,6 +1,7 @@
 package io.zerows.extension.module.modulat.component;
 
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.zerows.epoch.constant.KName;
 import io.zerows.epoch.store.jooq.DB;
@@ -12,15 +13,18 @@ import io.zerows.extension.module.modulat.serviceimpl.BagArgService;
 import io.zerows.extension.module.modulat.servicespec.BagArgStub;
 import io.zerows.extension.skeleton.common.enums.TypeBag;
 import io.zerows.platform.enums.modeling.EmModel;
+import io.zerows.support.Fx;
 import io.zerows.support.Ut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 public abstract class EquipForBase implements EquipFor {
@@ -45,19 +49,82 @@ public abstract class EquipForBase implements EquipFor {
             if (Objects.isNull(bags) || bags.isEmpty()) {
                 return Future.succeededFuture(new ConcurrentHashMap<>());
             }
-            return null;
+            // 先正向分组提取 bagId -> store 的映射关系，store 必须有值
+            final List<BBag> bagList = new ArrayList<>(bags);
+            final ConcurrentMap<String, String> storeMap = new ConcurrentHashMap<>();
+            bags.forEach(bag -> {
+                final String store = this.lookupStore(bag, bagList);
+                if (Ut.isNotNil(store)) {
+                    storeMap.put(bag.getId(), store);
+                }
+            });
+
+
+            // 逆向构造
+            final Map<String, List<BBag>> revertMap = new ConcurrentHashMap<>();
+            storeMap.forEach((bagId, store) -> {
+                final BBag bag = bagList.stream()
+                    .filter(item -> bagId.equals(item.getId()))
+                    .findFirst().orElse(null);
+                if (Objects.nonNull(bag)) {
+                    revertMap.computeIfAbsent(store, key -> new ArrayList<>()).add(bag);
+                }
+            });
+            return Future.succeededFuture(revertMap);
         });
+    }
+
+    private String lookupStore(final BBag bag, final List<BBag> bagList) {
+        String store = bag.getStore();
+        if (Ut.isNil(store) && Ut.isNotNil(bag.getParentId())) {
+            final BBag parent = bagList.stream()
+                .filter(item -> bag.getParentId().equals(item.getId()))
+                .findFirst().orElse(null);
+            if (Objects.nonNull(parent)) {
+                store = this.lookupStore(parent, bagList);
+            }
+        }
+        return store;
     }
 
     protected Logger log() {
         return LoggerFactory.getLogger(this.getClass());
     }
 
-    protected Future<JsonObject> dataAsync(final BBag bag) {
+    private Future<JsonObject> dataAsync(final BBag bag, final boolean isOpen) {
         final BagArgStub stub = Ut.singleton(BagArgService.class);
         return stub.seekBlocks(bag).compose(blocks -> {
             final Combiner<BBag, List<BBlock>> combiner = Combiner.forBlock();
             return combiner.configure(bag, blocks);
+        }).map(data -> {
+            final JsonObject dataJ = data.copy();
+            final JsonArray uiOpen = bag.getUiOpen();
+            if (isOpen && Ut.isNotNil(uiOpen)) {
+                final Set<String> openSet = Ut.toSet(uiOpen);
+                openSet.forEach(dataJ::remove);
+            }
+            return dataJ;
+        });
+    }
+
+    private Future<JsonObject> dataAsync(final List<BBag> bagList, final boolean isOpen) {
+        final List<Future<JsonObject>> futureL = new ArrayList<>();
+        bagList.forEach(bag -> futureL.add(this.dataAsync(bag, isOpen)));
+        return Fx.combineT(futureL).compose(dataStore -> {
+            // 值合并
+            final JsonObject combineJ = new JsonObject();
+            dataStore.forEach(item -> combineJ.mergeIn(item, true));
+            return Future.succeededFuture(combineJ);
+        });
+    }
+
+    protected Future<JsonObject> dataAsync(final Map<String, List<BBag>> bagMap, final boolean isOpen) {
+        final ConcurrentMap<String, Future<JsonObject>> futureMap = new ConcurrentHashMap<>();
+        bagMap.forEach((store, bagList) -> futureMap.put(store, this.dataAsync(bagList, isOpen)));
+        return Fx.combineM(futureMap).map(stored -> {
+            final JsonObject result = new JsonObject();
+            stored.forEach(result::put);
+            return result;
         });
     }
 }
--- zero-ecosystem/zero-plugins-extension/zero-exmodule-modulat/zero-exmodule-modulat-provider/src/main/java/io/zerows/extension/module/modulat/component/EquipForOpen.java
+++ zero-ecosystem/zero-plugins-extension/zero-exmodule-modulat/zero-exmodule-modulat-provider/src/main/java/io/zerows/extension/module/modulat/component/EquipForOpen.java
@@ -1,12 +1,24 @@
 package io.zerows.extension.module.modulat.component;
 
+import io.r2mo.typed.cc.Cc;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.zerows.platform.enums.modeling.EmModel;
 
 public class EquipForOpen extends EquipForBase {
+    private static final Cc<String, JsonObject> OPEN_DATA = Cc.open();
+
     @Override
     public Future<JsonObject> configure(final String appId, final EmModel.By by) {
-        return null;
+        if (OPEN_DATA.containsKey(appId)) {
+            return Future.succeededFuture(OPEN_DATA.get(appId));
+        }
+        final JsonObject condition = this.buildQr(appId, by);
+        return this.fetchBags(condition)
+            .compose(map -> this.dataAsync(map, true))
+            .map(result -> {
+                OPEN_DATA.put(appId, result);
+                return result;
+            });
     }
 }
