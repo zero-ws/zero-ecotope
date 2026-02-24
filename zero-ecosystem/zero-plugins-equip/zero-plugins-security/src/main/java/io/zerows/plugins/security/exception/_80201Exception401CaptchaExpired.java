d11579e2b44e3aa78234d228caf98122f8856975
Lang <silentbalanceyh@126.com>
2026-02-24 18:33:00 +0800

    CaptchaService

--- zero-ecosystem/zero-plugins-equip/zero-plugins-security/src/main/java/io/zerows/plugins/security/exception/_80201Exception401CaptchaExpired.java
+++ zero-ecosystem/zero-plugins-equip/zero-plugins-security/src/main/java/io/zerows/plugins/security/exception/_80201Exception401CaptchaExpired.java
@@ -3,8 +3,8 @@ package io.zerows.plugins.security.exception;
 import io.r2mo.vertx.common.exception.VertxWebException;
 
 public class _80201Exception401CaptchaExpired extends VertxWebException {
-    public _80201Exception401CaptchaExpired(final String clientId,
+    public _80201Exception401CaptchaExpired(final String captchaId,
                                             final String captcha) {
-        super(ERR._80201, clientId, captcha);
+        super(ERR._80201, captchaId, captcha);
     }
 }
