9929e6ff2b5d44f00d940725233f729e8afb1eff
Lang <silentbalanceyh@126.com>
2026-03-06 11:00:24 +0800

    等待数据导入

---
+++ zero-ecosystem/zero-plugins-extension/previous/__oob/run-perm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+aj perm -r b0bc139a-9ea1-4642-b13e-23c9a45d4bf0 -o ADMIN.DEVELOPER -i LANG.YU    # 开发工程师
+aj perm -r 8c5b9d50-914a-496e-aa04-f7018dbb9db3 -o ADMIN.SUPERVISOR -i LANG.YU   # 超级管理员
+aj perm -r 2793890c-5e9e-440f-9541-4454b65264d7 -o ADMIN.SYSTEM -i LANG.YU       # 系统管理员
+aj perm -r ce7d1f66-86ed-4489-8c8f-9f5cf392f4cc -o ROOM.MANAGER -i LANG.YU       # 客房经理
+aj perm -r e5866dc5-9e70-4a5b-a7f4-7dc54fbdc86d -o ROOM.USER -i LANG.YU          # 客房人员
+aj perm -r 89b12b74-cdad-4b29-b127-7c092b23e03d -o FAN.MANAGER -i LANG.YU        # 财务经理
+aj perm -r 61111ec8-fd4b-4716-9e8f-d7cd69611f2d -o FAN.USER -i LANG.YU           # 财务人员
+aj perm -r 212a2ffc-e81b-436c-834a-7b400dfb671c -o SALE.MANAGER -i LANG.YU       # 销售经理
+aj perm -r 6f5d4e49-63b3-4218-961b-6a6e89f78836 -o SALE.USER -i LANG.YU          # 销售人员
+aj perm -r 5e48ee11-8baf-4128-8d82-e1d6655662c5 -o LOBBY.MANAGER -i LANG.YU      # 大堂经理
+aj perm -r 31c90b3b-c4e3-4e3c-9a36-5dacc3ee491d -o LOBBY.RECEIVER -i LANG.YU     # 接待人员
+aj perm -r c2ead81e-a78f-4b2a-ab1d-cb2fb540fb23 -o LOBBY.CASHIER -i LANG.YU      # 收银人员
+aj perm -r 97d6ec70-59fa-43db-bf75-8bc3735574e7 -o HOTEL.MANAGER -i LANG.YU      # 总经理
