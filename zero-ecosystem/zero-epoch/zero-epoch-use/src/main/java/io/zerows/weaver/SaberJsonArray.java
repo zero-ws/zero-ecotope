6ee6ff1a9bff79a8897ce254a9161e834d5278f3
unknown <627433532@qq.com>
2026-04-24 06:41:20 +0800

    解决windows下的生成代码问题

--- zero-ecosystem/zero-epoch/zero-epoch-use/src/main/java/io/zerows/weaver/SaberJsonArray.java
+++ zero-ecosystem/zero-epoch/zero-epoch-use/src/main/java/io/zerows/weaver/SaberJsonArray.java
@@ -1,6 +1,9 @@
 package io.zerows.weaver;
 
+import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.zerows.epoch.constant.KName;
 
 import java.util.function.Function;
 
@@ -16,6 +19,30 @@ class SaberJsonArray extends SaberJsonBase {
 
     @Override
     protected Function<String, JsonArray> getFun() {
-        return JsonArray::new;
+        return literal -> {
+            try {
+                return new JsonArray(literal);
+            } catch (final DecodeException ex) {
+                /*
+                 * Compatibility:
+                 * Some clients send JsonObject wrapper: { "data": [ ... ] }.
+                 * Keep old behaviour for real JsonArray literals, but unwrap when possible.
+                 */
+                try {
+                    final JsonObject wrapper = new JsonObject(literal);
+                    final JsonArray data = wrapper.getJsonArray(KName.DATA);
+                    if (data != null) {
+                        return data;
+                    }
+                    final JsonArray dataAlt = wrapper.getJsonArray("data");
+                    if (dataAlt != null) {
+                        return dataAlt;
+                    }
+                } catch (final DecodeException ignored) {
+                    // ignored
+                }
+                throw ex;
+            }
+        };
     }
 }
